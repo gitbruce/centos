2ef7ae7c8e713493e5597c89d9900a1457872aa3
gitbruce <mantianly@163.com>
2015-12-05 20:38:17 +0800

    first commit

---
+++ github.sh
@@ -0,0 +1,9 @@
+git config --global user.email "mantianly@163.com"
+git config --global user.name "gitbruce"
+
+git init
+git add startup.sh
+git commit -m "first commit"
+git remote add origin git@github.com:gitbruce/centos.git
+git push -u origin master
+
