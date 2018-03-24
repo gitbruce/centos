7270bcf3ce02ddf0ffa7c1fa926eb74f60c7b51c
gitbruce <mantianly@163.com>
2018-03-24 19:19:58 +0800

    remove file1.txt

--- github.sh
+++
@@ -1,9 +0,0 @@
-git config --global user.email "mantianly@163.com"
-git config --global user.name "gitbruce"
-
-git init
-git add startup.sh
-git commit -m "first commit"
-git remote add origin git@github.com:gitbruce/centos.git
-git push -u origin master
-
